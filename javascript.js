c6c11a77ba9e46d9e950d67064420d7921d2ca4c
vinothrobinson <vinothrobinson03@gmail.com>
2023-06-26 19:21:27 -0400

    Changed how the book information is displayed

--- javascript.js
+++ javascript.js
@@ -29,17 +29,36 @@ function displayLibrary() {
     const shelf = document.querySelector(".book-display")
     shelf.replaceChildren();
     for (let i = 0; i < myLibrary.length; i++) {
+        // Overall container for book information
         const book = document.createElement('div');
         book.classList.add("book");
         book.style.backgroundColor = "#d6d3d1"
+        // Individual divs for each section of a book
+        const title = document.createElement('div')
+        title.classList.add("title");
+        title.innerHTML = myLibrary[i].title
+        const author = document.createElement('div')
+        author.classList.add("author");
+        author.innerHTML = myLibrary[i].author
+        const pages = document.createElement('div')
+        pages.classList.add("pages");
+        pages.innerHTML = `${myLibrary[i].pages} pages`
+
+        // Adding the individual divs to the book container
+        book.appendChild(title)
+        book.appendChild(author)
+        book.appendChild(pages)
+        /*
+        const read = document.createElement('div')
+        read.classList.add("read")
         book.innerHTML = myLibrary[i].info();
+        */
         shelf.appendChild(book);
-        console.log("This worked?")
     }
 }
 
 
-const newBook = new Book("JJK", "Gege Akutami", "360", true)
+const newBook = new Book("JJK", "Gege Akutami", "325", true)
 console.log(newBook.info())
 newBook.addBookToLibrary();
 
@@ -50,3 +69,15 @@ newBook2.addBookToLibrary();
 const newBook3 = new Book("MHA", "Kohei Horikoshi", "370", false)
 console.log(newBook3.info())
 newBook3.addBookToLibrary();
+
+const newBook4 = new Book("Monster", "Naoki Urasawa", "100", true)
+console.log(newBook4.info())
+newBook4.addBookToLibrary();
+
+const newBook5 = new Book("Full Metal Alchemist", "Hiromu Arakawa", "200", true)
+console.log(newBook5.info())
+newBook5.addBookToLibrary();
+
+const newBook6 = new Book("Naruto", "Kishimoto", "700", false)
+console.log(newBook6.info())
+newBook6.addBookToLibrary();
